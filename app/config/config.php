83045c37d57c9784f4614a0e7395a9c62125de06
Thomas Braakman 336469 <336469@student.mboutrecht.nl>
2023-10-23 18:49:09 +0200

    commit-3

--- app/config/config.php
+++ app/config/config.php
@@ -3,5 +3,5 @@
 define('DB_HOST', 'localhost');
 define('DB_USER', 'root');
 define('DB_PASS', '');
-define('DB_NAME', 'be-l2-databas');
+define('DB_NAME', 'be-l2-database');
 define('URLROOT', 'http://www.be-opdracht2-p1-l2.org');
